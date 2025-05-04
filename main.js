1fbf3cae1c2261e855596e1ae66940cd3df38888
AhmedY29 <ahmedys2001@gmail.com>
2025-05-04 11:09:22 +0300

    complate lab

--- main.js
+++ main.js
@@ -30,3 +30,22 @@ var bill = 10.25 + 3.99 + 7.15,
   total = bill * tip;
 
 console.log(total);
+
+// 6 mad libs
+
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+console.log(
+  `The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content!`
+);
+
+// 7 Awesome Message
+
+var firstName = "ahmed";
+var interest = "programming";
+var hobby = "football";
+
+var awesomeMessage = `Hi, my name is ${firstName}. I love ${interest}. In my spare time, I like to ${hobby}.`;
+
+console.log(awesomeMessage);
